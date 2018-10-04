6fce4cfabe5a4bc4a9cae5de9bfb17c700244fcd
Dushyant Dubaria <dushyant.dubaria2010@gmail.com>
2018-10-04 02:41:45 -0500

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -57,7 +57,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <div class="row">
 		
 	<div class="row">
-                <h5><i>...     It's not about money, it's about sending a message. Everythings burns!!! -The Dark Knight</h5>
+                <h5><i>...It's not about money, it's about sending a message. Everythings burns!!! -The Dark Knight</h5>
 	</i></div>
         <div class="row">
 
