5b907426439b302dcc0e2cd8c504fa9e706ef382
pushmessi10 <43080504+pushmessi10@users.noreply.github.com>
2018-11-10 20:19:58 -0600

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,66 +1,46 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Dushyant Dubaria</title>
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/jquery-ui.min.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/styles.css">
-	<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.min.js"></script>
-	<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery-ui.min.js"></script>
-	<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery-ui.theme.min.js"></script>
-	<script type="text/javascript" src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="<?php echo base_url(); ?>js/script.js"></script>
+
+
+<html><head>
+<title>validation</title>
+ <script language="javascript">
+ function verify() {
+ if (document.form1.Name.value == "")
+     {
+                alert("Please give the name");
+                document.form1.Name.focus();
+                return false;
+            }
+  if (document.form1.Address.value == "") 
+            {
+                alert("Please give address");
+                document.form1.Address.focus();
+                return false;
+            }
+ return( true );
+}
+</script> 
 </head>
-<body>
-<div id="header">
-	
-</div>
-<div class="container">
-	<div class="row">
-		<p>Please fill the following form to book a  ticket</p>
-	</div>
-	<div class="row">
-	<form action="/welcome/book" method="POST">
-	  <div class="form-group">
-		<label for="exampleInputEmail1">Email address</label>
-		<input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-	  </div>
-	  <div class="form-group">
-		<label for="passengers">Number of passengers</label>
-		<select class="form-control" name="passengers" id="passengers">
-		  <option>1</option>
-		  <option>2</option>
-		  <option>3</option>
-		  <option>4</option>
-		</select>
-	  </div>
-	  <div class="form-group">
-	  	<label for="departure">Departure date</label>
-	  	<input type="text" id="datepicker" name="departure">
-	  </div>
-	  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-	</div>
-	</div>
-	</div>
-	</div>
-	<div class="row">
-                <h5><i>...    You either die a hero or you live long enough to see yourself become the villain.</h5>
-	</i></div>
-        <div class="row">
-		
-	<div class="row">
-                <h5><i>......    It's not about money, it's about sending a message. Remember everything burns away!!!</h5>
-	</i></div>
-        <div class="row">
-
-</form>
-	</div>
-</div>
-
-</body>
-</html>
-
+<!-- Creating Form -->
+<body bgcolor="red"><center><font size=10>
+<form method="POST" action="registration.php" name="form1">
+<b><h3>REGISTRATION FORM</h3></b>
+<table border=1 align=center bgcolor="Lavender" cellpadding=20px><br>
+<td colspan=6 align=center><b>STUDENT INFORMATION</td> 
+<form name ="registration Form" action="successstud.php " method="POST"><tr>
+<td>Name:</td><td> <input type = "text" name = "Name" size=30 maxlength=30></td></tr>
+<tr><td>Phone_No :</td> <td><input type = "text" name = "Ph_no" size=12 maxlength=12></td></tr>
+<tr><td>Address :</td><td><textarea name="Address" id="Address" size=30 maxlength=30></textarea> </td></tr>
+<tr><td>email_id:</td><td> <input type = "text" name = "eid" size=30 maxlength=30></td></tr>
+<tr><td>Date:</td><td><textarea name="date" rows="2" cols="20"></textarea></td></tr>
+<tr><td> Dance_Type:<td><select name="dance_type"> 
+<option value="-1" selected>select..</option> 
+<option value="Classical">Classical</option> 
+<option value="Hip-Hop">Hip-Hop</option>
+<option value="Bollywood">Bollywood</option> 
+<option value="Jazz">Jazz</option></select></td> </td></tr> 
+<tr><td colspan =2 align="center"> 
+<button type="submit">submit</button>
+<button type="Reset">Reset</button></td></tr></table><br /><br />
